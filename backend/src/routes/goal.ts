677bfd2e20ae65bb2dbf95e52845f7ad8d99edba
AayushShrestha6163 <aayushshrestha6163@gmail.com>
2026-06-16 21:27:30 +0545

    goals upadate and delete

--- backend/src/routes/goal.ts
+++ backend/src/routes/goal.ts
@@ -38,6 +38,119 @@ router.post(
   }
 );
 
+// GET ALL GOALS
+
+router.get(
+  "/",
+  authMiddleware,
+  async (req: AuthRequest, res) => {
+
+    try {
+
+      const goals = await Goal.find({
+        userId: req.user?.id
+      });
+
+
+      res.status(200).json(goals);
+
+
+    } catch (error) {
+
+      res.status(500).json({
+        message:"Failed to fetch goals"
+      });
+
+    }
+
+  }
+);
+
+// UPDATE GOAL
+
+router.put(
+  "/:id",
+  authMiddleware,
+  async (req: AuthRequest, res) => {
+
+    try {
+
+      const goal = await Goal.findOneAndUpdate(
+        {
+          _id:req.params.id,
+          userId:req.user?.id
+        },
+        req.body,
+        {
+          new:true
+        }
+      );
+
+
+      if(!goal){
+        return res.status(404).json({
+          message:"Goal not found"
+        });
+      }
+
+
+      res.status(200).json(goal);
+
+
+    } catch(error){
+
+      res.status(500).json({
+        message:"Failed to update goal"
+      });
+
+    }
+
+  }
+);
+
+// DELETE GOAL
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  async (req:AuthRequest,res)=>{
+
+
+    try{
+
+
+      const goal = await Goal.findOneAndDelete({
+        _id:req.params.id,
+        userId:req.user?.id
+      });
+
+
+
+      if(!goal){
+        return res.status(404).json({
+          message:"Goal not found"
+        });
+      }
+
+
+
+      res.json({
+        message:"Goal deleted successfully"
+      });
+
+
+
+    }catch(error){
+
+      res.status(500).json({
+        message:"Failed to delete goal"
+      });
+
+    }
+
+
+  }
+);
 
 
 export default router;
